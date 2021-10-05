0c4894bf01d2fe5d1961af5d373c5a1b83e6157a
esther-v <esvalas@gmail.com>
2021-10-05 22:47:23 +0200

    starting project 💥

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: 'what is the most popular programming language in 2021',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'Javascript',
+        correct: 'd'
+    },
+    {
+        question: 'who is the President of US ? ',
+        a: 'Hillary Clinton',
+        b: 'Donald Trump',
+        c: 'Joe Biden',
+        d: 'Barack Obama',
+        correct: 'c'
+    },
+    {
+        question: 'what does HTML stand for ?',
+        a: 'Hypertext Markup Language',
+        b: 'Helicopters Terminals Motorbeats Lamborghinis',
+        c: 'High Technology Modern Language',
+        d: 'Hide The Missing Library',
+        correct: 'a'
+    },
+    {
+        question: 'what is the main function of CSS ?',
+        a: 'color the text',
+        b: 'apply consistent styling of elements across all pages of the site',
+        c: 'change background',
+        d: 'move elements',
+        correct: 'b'
+    },
+    {
+        question: 'what year was javascript launched',
+        a: '2006',
+        b: '2003',
+        c: '1995',
+        d: '1999',
+        correct: 'c'
+    }
+]
