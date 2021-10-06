5ad7a5ae4758680fac461683e418aadf2e36eb76
esther-v <esvalas@gmail.com>
2021-10-06 18:02:48 +0200

    logic ok

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const quizData = [
     {
-        question: 'what is the most popular programming language in 2021',
+        question: 'What is the most popular programming language in 2021',
         a: 'Java',
         b: 'C',
         c: 'Python',
@@ -8,7 +8,7 @@ const quizData = [
         correct: 'd'
     },
     {
-        question: 'who is the President of US ? ',
+        question: 'Who is the President of US ? ',
         a: 'Hillary Clinton',
         b: 'Donald Trump',
         c: 'Joe Biden',
@@ -16,7 +16,7 @@ const quizData = [
         correct: 'c'
     },
     {
-        question: 'what does HTML stand for ?',
+        question: 'What does HTML stand for ?',
         a: 'Hypertext Markup Language',
         b: 'Helicopters Terminals Motorbeats Lamborghinis',
         c: 'High Technology Modern Language',
@@ -24,7 +24,7 @@ const quizData = [
         correct: 'a'
     },
     {
-        question: 'what is the main function of CSS ?',
+        question: 'What is the main function of CSS ?',
         a: 'color the text',
         b: 'apply consistent styling of elements across all pages of the site',
         c: 'change background',
@@ -32,11 +32,79 @@ const quizData = [
         correct: 'b'
     },
     {
-        question: 'what year was javascript launched',
+        question: 'What year was javascript launched',
         a: '2006',
         b: '2003',
         c: '1995',
         d: '1999',
         correct: 'c'
     }
-]
+];
+const answerEls = document.querySelectorAll('.answer');
+const quizz = document.getElementById('quizz');
+const questionText = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+
+let currentQuizz = 0;
+let score = 0;
+
+loadQuizz();
+
+function loadQuizz() {
+    deselectAnswer();
+
+    const currentQuizzData = quizData[currentQuizz];
+    questionText.innerText = currentQuizzData.question;
+    
+    a_text.innerText = currentQuizzData.a;
+    b_text.innerText = currentQuizzData.b;
+    c_text.innerText = currentQuizzData.c;
+    d_text.innerText = currentQuizzData.d;
+
+    
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked){
+            answer = answerEl.id;
+        }
+    }) 
+
+    return answer;
+}
+
+function deselectAnswer(){
+    answerEls.forEach(answerEl => {
+        answerEl.checked = false;   
+    })
+}
+
+submitBtn.addEventListener('click', () => {
+
+    const answer = getSelected();
+    
+
+    if(answer) {
+        if(answer === quizData[currentQuizz].correct){
+            score++;
+        }
+        currentQuizz++;
+        if(currentQuizz < quizData.length) {
+            loadQuizz();
+        } else {
+            quizz.innerHTML = `
+                <h3>You answered correctly at ${score}/${quizData.length} questions </h3> 
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+   
+})
